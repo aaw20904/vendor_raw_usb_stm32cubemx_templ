66590b39896bdba64bc23c735737939229916641
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2025-12-22 12:46:20 +0200

    Update instruction for integrate.c

--- instruction for integrate.c
+++ instruction for integrate.c
@@ -3,5 +3,6 @@
 There are the next steps to create custom  "vendor" class with CubeMX
 1)Create a project, enable midddleware, choose "Custom HID class"
 2) Remove "usbd_custom_hid_if.c", "usbd_custom_hid_if.h" 
+   Custom HID must be deleted completely
 3)
 */
